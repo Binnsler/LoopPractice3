60313b8c9470c600c411877fb0fb21e71aac5604
Jamie Binns <jamie.m.binns@gmail.com>
2015-07-02 12:00:57 -0600

    Finished exercises

--- main.js
+++ main.js
@@ -40,11 +40,41 @@
 
 //  Problem 2  //
 
-var parseNameCity = function(array){
-	array.forEach(function(student){
-		console.log(student.age, student.city);
+// var parseNameCity = function(array){
+// 	array.forEach(function(student){
+// 		console.log(student.age, student.city);
 
-	});
-}
+// 	});
+// }
 
-parseNameCity(students)
+// parseNameCity(students)
+
+
+//  Problem 3  //
+
+// var parseNameCity = function(array){
+// 	array.forEach(function(student){
+// 		if(student.city === 'Boulder'){
+// 			console.log(student.name + ' is from ' + student.city);
+// 	}
+
+// 	});
+// }
+
+// parseNameCity(students);
+
+
+
+//  Problem 4  //
+
+// var parseNameAge = function(array){
+// 	array.forEach(function(student){
+// 		if(student.age > 25){
+// 			console.log(student.name + ' is older than 25');
+// 	}
+
+// 	});
+// }
+
+// parseNameAge(students);
+
